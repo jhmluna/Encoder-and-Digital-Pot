ad9619b2276801c6539096252ec544867dfcecb5
José Henrique de Melo Luna <jhmluna@gmail.com>
2023-01-30 16:07:58 -0300

    Update Encoder-and-Digital-Pot.ino

--- Encoder-and-Digital-Pot.ino
+++ Encoder-and-Digital-Pot.ino
@@ -1,6 +1,6 @@
 // -----
 // Programa de exemplo baseado em InterruptRotator.ino da biblioteca RotaryEncoder.
-// Também utiliza a biblioteca X9C10X para o potênciômetro digital X9C10X .
+// Também utiliza a biblioteca X9C10X para o voltage_coarseênciômetro digital X9C10X .
 // Esta classe é implementada para uso no ambiente do Arduino.
 // -----
 // 25/01/2023 - Início
@@ -21,13 +21,14 @@
 #include <Arduino.h>
 #include <RotaryEncoder.h>
 #include "X9C10X.h"
+#include <JC_Button.h>          // https://github.com/JChristensen/JC_Button
 
-X9C10X pot(10000);  // Initializes the resistance to 10 KΩ
-
+// Encoder pin assignments and setup
+const byte ENCODER_BUTTON_PIN(5);              // connect a button switch from this pin to ground
+Button encoder_Btn(ENCODER_BUTTON_PIN);       // define the button
 // A pointer to the dynamic created rotary encoder instance.
 // This will be done in setup()
 RotaryEncoder *encoder = nullptr;
-
 // Encoder input pins
 #define PIN_IN1 6
 #define PIN_IN2 7
@@ -38,15 +39,20 @@ ISR(PCINT2_vect) {
   encoder->tick(); // just call tick() to check the state.
 }
 
+// Digital pot setup
+X9C104 voltage_coarse(100000);  // Initializes the resistance to 100 KΩ
+X9C103 voltage_fine(10000);     // Initializes the resistance to 10 KΩ
+
 void setup() {
   Serial.begin(115200);
-  while (!Serial)
-    ;
+  while (!Serial);
   Serial.println("InterruptRotator example for the RotaryEncoder library.");
 
-  // Digital Pot configuration
-  pot.begin(2, 3, 4);        // (pulse, direction, select)
-  pot.setPosition(0, true);  // The wiper will be moved to the closest "end" position and from there moved to the 0 position.
+  // Digital pot configuration
+  voltage_coarse.begin(2, 3, 4);        // (pulse, direction, select)
+  voltage_coarse.setPosition(0, true);  // The wiper will be moved to the closest "end" position and from there moved to the 0 position.
+  voltage_fine.begin(2, 3, 8);        // (pulse, direction, select)
+  voltage_fine.setPosition(0, true);  // The wiper will be moved to the closest "end" position and from there moved to the 0 position.
 
   // Setup the rotary encoder functionality
   // Use TWO03 mode when PIN_IN1, PIN_IN2 signals are both LOW or HIGH in latch position.
@@ -56,27 +62,39 @@ void setup() {
   // Here is some code for D6 and D7 using ATMega328P specific registers.
 
   // Setup flags to activate the ISR PCINT2.
-    PCICR |= (1 << PCIE2);    // This enables Pin Change Interrupt 2 that covers the Digital input pins or Port D.
-    PCMSK2 |= (1 << PCINT22) | (1 << PCINT23);  // This enables the interrupt for pin 6 and 7 of Port D.
-}  // setup()
+  PCICR |= (1 << PCIE2);    // This enables Pin Change Interrupt 2 that covers the Digital input pins or Port D.
+  PCMSK2 |= (1 << PCINT22) | (1 << PCINT23);  // This enables the interrupt for pin 6 and 7 of Port D.
 
+  encoder_Btn.begin();              // initialize the button object
+}  // setup()
 
 // Read the current position of the encoder and print out when changed.
 void loop() {
   static int pos = 0;
 
+  static int encoder_step = 1;  // a variable that keeps the rotary encoder step  
+  static bool button_state;         // a variable that keeps the current LED status
+  encoder_Btn.read();         // read the button
+
   int newPos = encoder->getPosition();
   if (pos != newPos) {
     int direction = (int)(encoder->getDirection());
     if (direction > 0) {
-      pot.incr();
+      voltage_coarse.incr(encoder_step);
     } else {
-      pot.decr();
+      voltage_coarse.decr(encoder_step);
     }
     Serial.print("pos:");
     Serial.print(newPos);
     Serial.print(" dir:");
     Serial.println(direction);
     pos = newPos;
+    Serial.println(encoder_step);
   }  // if
-}  // loop ()
+
+  if (encoder_Btn.wasReleased())    // if the button was released, change the LED state
+  {
+    button_state = !button_state;
+    encoder_step = button_state == HIGH ? 5 : 1;
+  }
+} // loop()
